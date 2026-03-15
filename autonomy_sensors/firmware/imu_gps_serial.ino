ef6b390792dbb3d70fa9ed3992055944022954b0
Artemis Lee <96yrlee@gmail.com>
2026-03-15 18:17:48 -0400

    Refactor IMU and GPS integration in firmware
    Restored Wire.h inclusion and removed SPI configuration for ICM_20948. Updated setup and loop functions for IMU and GPS integration.

--- autonomy_sensors/firmware/imu_gps_serial.ino
+++ autonomy_sensors/firmware/imu_gps_serial.ino
@@ -13,37 +13,26 @@
 #include "ICM_20948.h" //Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 // I also just flat out included the src folder, but will assume we're installing the actual library @ https://github.com/sparkfun/SparkFun_ICM-20948_ArduinoLibrary
 
-// #include <Wire.h>      // removed as already defined in firmware/src/ICM_20948.h
+#include <Wire.h>      // placed it back in, but depending on how it goes maybe comment out again
 #include <TinyGPS++.h>
 
-#define WIRE_PORT Wire    // Your desired Wire port.      Used when "USE_SPI" is not defined
+
 #define SERIAL_PORT Serial
 
 /* =========================
    IMU CONFIG
    ========================= */
-//#define USE_SPI       // Uncomment this to use SPI
-// #define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
-// #define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
+ICM_20948_I2C myICM; // removed the SPI configuration
 
-#ifdef USE_SPI
-   ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
-#else
-   ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
-#endif
+#define WIRE_PORT Wire    
+// For the ICM_20948 object: The value of the last bit of the I2C address.
+// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define AD0_VAL 1
 
-// from old IMU sensors, not sure if PINS still required
-//#define MPU_ADDR 0x68
-//#define SDA_PIN 21
-//#define SCL_PIN 22
-
-//const float ACC_SCALE  = 16384.0f;
-//const float GYRO_SCALE = 131.0f;
-//const float G = 9.80665f;
-
-/* Biases */
-//float gbx=0, gby=0, gbz=0;
-//float abx=0, aby=0, abz=0;
+// ESP32 needs the pins and address? but the sparkfun ICM_20948.cpp/h object appaers to handle the wire part...
+#define IMU_ADDR 0x68 
+#define SDA_PIN 21
+#define SCL_PIN 22
 
 /* Timing */
 const unsigned long IMU_PERIOD_MS = 10;    // 100 Hz
@@ -63,6 +52,34 @@ unsigned long last_gps_time = 0;
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(2);
 
+/* =========================
+   SETUP
+   ========================= */
+void setup() {
+  //serial monitor from ESP32
+  Serial.begin(115200);
+  delay(2000);
+
+  // GPS setup
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("# GPS serial started");
+
+  // IMU setup
+  Wire.begin(SDA_PIN, SCL_PIN); 
+  Wire.setClock(400000);
+   
+  //wake_mpu(), not required with ICM_20948_I2C object, i think
+  /*
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(0x6B); //the PDF has more deets on the actual start sequence but man...
+  Wire.write(0x00);
+  Wire.endTransmission(); 
+  delay(50);
+  */
+  setup_imu();
+  
+  Serial.println("# IMU + GPS streaming started");
+} //END setup()
 
 void setup_imu(){
   // from Example1_Basics.ino, Example2_advanced.ino  
@@ -71,14 +88,12 @@ void setup_imu(){
 
   bool initialized = false;
   while (!initialized){
-    #ifdef USE_SPI
-      myICM.begin(CS_PIN, SPI_PORT);
-    #else
-      myICM.begin(Wire, AD0_VAL);
-    #endif
+    myICM.begin(Wire, AD0_VAL); //handles the wake_mpu() stuff??? as well as some calibration apparently??? 
+     //its complex in there i aint touching it... esp with DMP involve, which we'll likely want for quartenionr or what its spelled
 
     Serial.print(F("Initialization of the IMU sensor returned: "));
     Serial.println(myICM.statusString());
+     
     if (myICM.status != ICM_20948_Stat_Ok){ // failed to start imu
       Serial.println("Trying again...");
       delay(500);
@@ -89,34 +104,9 @@ void setup_imu(){
   }// END while loop  
    
   // advanced has more setup but idc right now
+  // https://github.com/sparkfun/SparkFun_ICM-20948_ArduinoLibrary/blob/main/examples/Arduino/Example2_Advanced/Example2_Advanced.ino
    
 } // END setup_imu()
-
-/* =========================
-   SETUP
-   ========================= */
-void setup() {
-  Serial.begin(115200);
-  delay(2000);
-
-  //due to IMU
-  #ifdef USE_SPI
-    SPI_PORT.begin();
-  #else
-    Wire.begin();
-    //Wire.begin(SDA_PIN, SCL_PIN); // the SDA and SLA pins for MPU 6050 module, old IMU
-    Wire.setClock(400000);
-  #endif
-  
-  // start GPS unit
-  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("# GPS serial started");
-  
-  // start IMU
-  setup_imu();
-  
-  Serial.println("# IMU + GPS streaming started");
-} //END setup()
 
 /* =========================
    LOOP
@@ -124,15 +114,17 @@ void setup() {
 void loop() {
   /* Always parse GPS bytes */
   while (gpsSerial.available()) {
-  gps.encode(gpsSerial.read());
+    gps.encode(gpsSerial.read());
   }
    
   unsigned long now = millis();
   
   /* -------- IMU OUTPUT -------- */
   if (now - last_imu_time >= IMU_PERIOD_MS) {
+    //'header' field to indicate this is IMU data, and timestampe for topic info 
     Serial.print("IMU,");
     Serial.print(now); Serial.print(",");
+     
     // start IMU serial print
     if (myICM.dataReady()){
       myICM.getAGMT();             // The values are only updated when you call 'getAGMT'
@@ -142,6 +134,7 @@ void loop() {
     }
     else{
       Serial.print("nan,nan,nan,nan,nan,nan,nan,nan,nan,");
+      // SERIAL_PORT.println("Waiting for IMU data: myICM.dataReady() == False");
       delay(500);
     }
     Serial.println(""); //IMU stops printing
@@ -175,13 +168,8 @@ void loop() {
   
 }//END loop()
 
-#ifdef USE_SPI
-void printScaledAGMT(ICM_20948_SPI *sensor)
-{
-#else
-void printScaledAGMT(ICM_20948_I2C *sensor)
-{
-#endif
+//From Example1_Basics.ino printing helper, modified to match old format
+void printScaledAGMT(ICM_20948_I2C *sensor){
   printFormattedFloat(sensor->accX(), 4, 2); Serial.print(",");
   printFormattedFloat(sensor->accY(), 4, 2); Serial.print(",");
   printFormattedFloat(sensor->accZ(), 4, 2); Serial.print(",");
@@ -195,9 +183,8 @@ void printScaledAGMT(ICM_20948_I2C *sensor)
   printFormattedFloat(sensor->magZ(), 4, 2);
 }
 
-// helper function from example1, to print the imu data
-void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
-{
+// helper function from Example1_Basics.ino, to print the imu data
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals){
   float aval = abs(val);
   if (val < 0)  {
     Serial.print("-");
@@ -207,7 +194,7 @@ void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
   }
   for (uint8_t indi = 0; indi < leading; indi++){
     uint32_t tenpow = 0;
-    if (indi < (leading - 1))    {
+    if (indi < (leading - 1)){
       tenpow = 1;
     }
     for (uint8_t c = 0; c < (leading - 1 - indi); c++){
@@ -219,7 +206,8 @@ void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
     else{
       break;
     }
-  }//END for loop
+  }//END outerfor loop
+   
   if (val < 0){
     Serial.print(-val, decimals);
   }
