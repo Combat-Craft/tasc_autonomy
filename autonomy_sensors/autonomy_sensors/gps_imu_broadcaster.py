3b002e18d7ee33fc82bc367583474bbec4902962
Artemis Lee <96yrlee@gmail.com>
2026-03-18 02:52:09 -0400

    added foxglove textannotation msgs
    Added Foxglove Text Annotations for GPS and heading information.

--- autonomy_sensors/autonomy_sensors/gps_imu_broadcaster.py
+++ autonomy_sensors/autonomy_sensors/gps_imu_broadcaster.py
@@ -13,6 +13,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Imu, MagneticField, NavSatFix, NavSatStatus
 from geometry_msgs.msg import Quaternion
 from std_msgs.msg import Header
+from foxglove_msgs import TextAnnotation
 
 from math import atan2, PI, sqrt     # for heading, roll, pitch i.e. "compass" angles
 
@@ -53,6 +54,10 @@ class SerialImuGpsNode(Node):
         self.gps_pub = self.create_publisher(NavSatFix, '/gps/fix', 10)
         self.heading_pub = self.create_publisher(Float32, '/heading', 10) #
         self.compass_pub = self.create_publisher(String, '/cardinal_compass', 10) # N S E W
+
+        self.latlon_pub = self.create_publisher(TextAnnotation, '/cam_overlay/latitude_longitude', 10)
+        self.headfox_pub = self.create_publisher(TextAnnotation, '/cam_overlay/heading', 10)
+        self.cardinalcompass_pub = self.create_publisher(TextAnnotation, '/cam_overlay/compass', 10)
                                                
         # Serial
         try:
@@ -157,8 +162,8 @@ class SerialImuGpsNode(Node):
         # Publish Heading (commented out: pitch and roll) ================================================
         DECLINATION 10.05 # Declination (degrees) in Boulder, CO.
         
-        //roll = atan2(ay, az)
-        //pitch = atan2(-ax, sqrt(ay * ay + az * az))
+        #roll = atan2(ay, az)
+        #pitch = atan2(-ax, sqrt(ay * ay + az * az))
         
         heading = 0
         if (my == 0):
@@ -177,22 +182,56 @@ class SerialImuGpsNode(Node):
         elif (heading < -PI):
             heading += (2 * PI)
         
-        // Convert everything from radians to degrees:
+        # Convert everything from radians to degrees:
         heading *= 180.0 / PI
-        //pitch *= 180.0 / PI
-        //roll  *= 180.0 / PI
+        #pitch *= 180.0 / PI
+        #roll  *= 180.0 / PI
 
         heading_msg = Float32()
         heading_msg.data = heading
         self.heading_pub.publish(heading_msg)
+
+        # Publish Foxglove Text Annotation for heading =====================
+        headingFoxglove_msg = TextAnnotation()
+        headingFoxglove_msg.timestamp = imu_msg.header.stamp
+        headingFoxglove_msg.position.x = 360 # origin is top left corner of the image
+        headingFoxglove_msg.position.y = 15
+        headingFoxglove_msg.text = str(heading)
+        headingFoxglove_msg.fontsize = 12
+        headingFoxglove_msg.text_color.r = 1
+        headingFoxglove_msg.text_color.g = 1
+        headingFoxglove_msg.text_color.b = 1
+        headingFoxglove_msg.text_color.a = 1 #opaque
+        headingFoxglove_msg.background_color.r = 0
+        headingFoxglove_msg.background_color.g = 0
+        headingFoxglove_msg.background_color.b = 0
+        headingFoxglove_msg.background_color.a = 0.01 #0 is transparent
+        self.headfox_pub.publish(headingFoxglove_msg)
 
         # Publish Cardinal Compass  ======================================================================
         ## will use 8-wind compass rose i.e. N NE E SE S SW W NW clockwise, 45deg each segment
         cardinal_dir = Cardinal_Direction_8(heading)
 
         compass_msg = String()
-        compass_msg.data = heading
+        compass_msg.data = cardinal_dir
         self.compass_pub.publish(compass_msg)
+
+        # Publish Foxglove Text Annotation for cardinal compass =====================
+        cardinalcompass_pub = TextAnnotation()
+        cardinalcompass_pub.timestamp = imu_msg.header.stamp
+        cardinalcompass_pub.position.x = 360 # origin is top left corner of the image
+        cardinalcompass_pub.position.y = 30
+        cardinalcompass_pub.text = cardinal_dir
+        cardinalcompass_pub.fontsize = 12
+        cardinalcompass_pub.text_color.r = 1
+        cardinalcompass_pub.text_color.g = 1
+        cardinalcompass_pub.text_color.b = 1
+        cardinalcompass_pub.text_color.a = 1 #opaque
+        cardinalcompass_pub.background_color.r = 0
+        cardinalcompass_pub.background_color.g = 0
+        cardinalcompass_pub.background_color.b = 0
+        cardinalcompass_pub.background_color.a = 0.1 #0 = transparent
+        self.headfox_pub.publish(headingFoxglove_msg)
 
         # END handle_imu()
 
@@ -205,36 +244,53 @@ class SerialImuGpsNode(Node):
         try:
             _, ms, lat, lon, alt, speed, hdop, sats, fix = line.split(',')
             fix = int(fix)
+        
+            msg = NavSatFix()
+            msg.header = Header()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = self.get_parameter('frame_id_gps').value
+    
+            if fix == 1:
+                msg.status.status = NavSatStatus.STATUS_FIX
+                msg.status.service = NavSatStatus.SERVICE_GPS
+    
+                msg.latitude = float(lat)
+                msg.longitude = float(lon)
+                msg.altitude = float(alt)
+    
+                msg.position_covariance = [
+                    65.7066, 0.0,    0.0,
+                    0.0,     65.7066,0.0,
+                    0.0,     0.0,    25.0    # (5 m)^2
+                ]
+                msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
+    
+            else:
+                msg.status.status = NavSatStatus.STATUS_NO_FIX
+                msg.status.service = NavSatStatus.SERVICE_GPS
+                msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
+    
+            self.gps_pub.publish(msg)
+    
+            # Publish Foxglove Text Annotation for cardinal compass =====================
+            latlon_pub = TextAnnotation()
+            latlon_pub.timestamp = imu_msg.header.stamp
+            latlon_pub.position.x = 360 # origin is top left corner of the image
+            latlon_pub.position.y = 15
+            latlon_pub.text = "lat: " + lat + "            " + "lon: " + lon #12 spaces   
+            latlon_pub.fontsize = 12
+            latlon_pub.text_color.r = 1
+            latlon_pub.text_color.g = 1
+            latlon_pub.text_color.b = 1
+            latlon_pub.text_color.a = 1 #opaque
+            latlon_pub.background_color.r = 0
+            latlon_pub.background_color.g = 0
+            latlon_pub.background_color.b = 0
+            latlon_pub.background_color.a = 0.1 #0 = transparent
+            self.headfox_pub.publish(headingFoxglove_msg)
+            
         except ValueError:
             return
-
-        msg = NavSatFix()
-        msg.header = Header()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = self.get_parameter('frame_id_gps').value
-
-        if fix == 1:
-            msg.status.status = NavSatStatus.STATUS_FIX
-            msg.status.service = NavSatStatus.SERVICE_GPS
-
-            msg.latitude = float(lat)
-            msg.longitude = float(lon)
-            msg.altitude = float(alt)
-
-            msg.position_covariance = [
-                65.7066, 0.0,    0.0,
-                0.0,     65.7066,0.0,
-                0.0,     0.0,    25.0    # (5 m)^2
-            ]
-            msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
-
-
-        else:
-            msg.status.status = NavSatStatus.STATUS_NO_FIX
-            msg.status.service = NavSatStatus.SERVICE_GPS
-            msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
-
-        self.gps_pub.publish(msg)
 
 
 def main():
