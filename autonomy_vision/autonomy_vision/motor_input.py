20633372ff30bfff33ccab0ded92abe0fb8b6737
Humaid <camosmilodon@gmail.com>
2026-07-01 14:34:17 -0400

    Updated motor_input.py to include pan trigger

--- autonomy_vision/autonomy_vision/motor_input.py
+++ autonomy_vision/autonomy_vision/motor_input.py
@@ -3,10 +3,26 @@
 """
 ROS2 Node
 
-motor_input.py is a ROS2 node that allows the user to enter servo angles within the range of -135 to +135 degrees.
-It publishes the command ot the /motor_cmd topic.
+Allows the user to control the camera servo and trigger a panorama sweep.
 
-ROS2 node 'motor_controller.py' subscribes to /motor_cmd and forwards the servo command to the Arduino.
+User inputs:
+    - A number from -135 to +135: 
+        Publishes a servo angle command
+    - 'p':
+        Triggers the panorama sweep
+    - 'e':
+        Exits the node
+
+Published topics:
+    - /motor_cmd
+    - /panorama_trigger
+
+Related nodes:
+    - motor_controller.py:
+        Subscribes to /motor_cmd and sends the converted servo command to Arduino.
+
+    - panorama_stitcher.py:
+        Subscribes to /panorama_trigger and begins the panorama sweep.
 """
 
 import rclpy
@@ -19,6 +35,8 @@ class MotorInput(Node):
 
         # Publisher
         self.pub = self.create_publisher(Float64, '/motor_cmd', 10)
+        self.pano_trigger_pub = self.create_publisher(Float64, '/panorama_trigger', 10)
+
 
     # Publish user command (target angle) to /motor_cmd
     def send(self, angle):
@@ -27,18 +45,28 @@ class MotorInput(Node):
         self.pub.publish(msg)
         self.get_logger().info(f"Sent: {angle}")
 
+    def trigger_panorama(self):
+        msg = Float64()
+        msg.data = 999.0
+        self.pano_trigger_pub.publish(msg)
+        self.get_logger().info("Panorama trigger sent")
+
+
 def main():
     rclpy.init()
     node = MotorInput()
     try:
         while True: # Recieves input from user continuously and it exits when user inputs 'e'
-            val = input("Enter angle (-135 to 135 | enter 'e' to exit): ") # Prompt user
+            val = input("Enter angle (-135 to 135 | 'p' for panorama | 'e' to exit): ") # Prompt user
             if val == 'e': # Exit
                 break
-            try: # Send command to ROS2 topic
-                node.send(float(val)) 
-            except ValueError: # Invalid
-                print("Invalid number")
+            elif val == 'p':
+                node.trigger_panorama()
+            else:
+                try: # Send command to ROS2 topic
+                    node.send(float(val)) 
+                except ValueError: # Invalid
+                    print("Invalid number")
     except KeyboardInterrupt:
         pass
     finally:
